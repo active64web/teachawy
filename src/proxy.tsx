ee3eff0894dc7f631cdadcf0795ea9d66d08e281
active4web <active64web@gmail.com>
2026-04-24 08:56:20 -0800

    hold website

--- src/proxy.tsx
+++ src/proxy.tsx
@@ -1,39 +1,39 @@
-// import createMiddleware from 'next-intl/middleware';
-// import { routing } from './i18n/routing';
-// import { NextResponse } from 'next/server';
-// import type { NextRequest } from 'next/server';
-
-// const intlMiddleware = createMiddleware(routing);
-
-// export default function middleware(request: NextRequest) {
-//     const url = request.nextUrl.clone();
-
-//     const isMaintenance = true;
-
-//     // سيب صفحة coming-soon تعدي
-//     if (url.pathname.includes('/coming-soon')) {
-//         return intlMiddleware(request);
-//     }
-
-//     // لو الموقع مقفول → حول
-//     if (isMaintenance) {
-//         url.pathname = '/coming-soon';
-//         return NextResponse.redirect(url);
-//     }
-
-//     // غير كده شغل middleware بتاع اللغة
-//     return intlMiddleware(request);
-// }
-
-// export const config = {
-//     matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
-// };
-
 import createMiddleware from 'next-intl/middleware';
 import { routing } from './i18n/routing';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export default function middleware(request: NextRequest) {
+    const url = request.nextUrl.clone();
+
+    const isMaintenance = true;
+
+    // سيب صفحة coming-soon تعدي
+    if (url.pathname.includes('/coming-soon')) {
+        return intlMiddleware(request);
+    }
+
+    // لو الموقع مقفول → حول
+    if (isMaintenance) {
+        url.pathname = '/coming-soon';
+        return NextResponse.redirect(url);
+    }
+
+    // غير كده شغل middleware بتاع اللغة
+    return intlMiddleware(request);
+}
 
 export const config = {
     matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
 };
+
+// import createMiddleware from 'next-intl/middleware';
+// import { routing } from './i18n/routing';
+
+// export default createMiddleware(routing);
+
+// export const config = {
+//     matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
+// };
